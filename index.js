f42a598daf22bb059ac49d0e801b4782acf7df56
Devin <devincb93@gmail.com>
2023-07-15 15:06:02 -0700

    Array Find Method

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(records) {
+    for (const obj of records){
+        if(obj.result === "W"){
+            return obj.year
+        }
+        }
+    }
